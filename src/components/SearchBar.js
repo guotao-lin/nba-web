c9706ee217c84933c7c407b6c28c6c76d5c23ac7
guotao <linguotao33511@gmail.com>
2017-12-23 18:30:54 -0500

    add searchBar enhangement

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { AutoComplete, Input, Icon } from 'antd';
 import nba from 'nba';
+import { PROFILE_PIC_URL_PREFIX } from '../constants';
+
+const Option = AutoComplete.Option;
 
 export class SearchBar extends React.Component {
   state = {
@@ -13,21 +16,34 @@ export class SearchBar extends React.Component {
 
   handleSearch = (value) => {
     this.setState({
-      dataSource: !value ? [] : nba.searchPlayers(value).map(player => player.fullName),
+      dataSource: !value ? [] : nba.searchPlayers(value).map(player => ({
+        playerId: player.playerId,
+        fullName: player.fullName,
+      })),
     });
   }
 
   render() {
     window.nba = nba;  // only for test
     const { dataSource } = this.state;
+    const options = dataSource.map(player => (
+      <Option key={player.fullName} text={player.fullName} className="player-option">
+        <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`} alt="Player" />
+        <span className="player-option-label">{player.fullName}</span>
+      </Option>
+    ));
+
+    console.log(dataSource);
+    console.log(options);
     return (
       <AutoComplete
         className="search-bar"
         size="large"
-        dataSource={dataSource}
+        dataSource={options}
         onSelect={this.onSelect}
         onSearch={this.handleSearch}
         placeholder="input here"
+        optionLabelProp="value"
       >
         <Input suffix={<Icon type="search" className="certain-category-icon" />} />
       </AutoComplete>
